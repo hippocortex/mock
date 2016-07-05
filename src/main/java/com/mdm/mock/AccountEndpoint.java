ba4de11637b12f8c71925dc6a39f715ea0f282b9
hippocortex <stephane.barberye@gmail.com>
2016-07-05 12:51:51 +0200

    affichage du nom de compte

--- src/main/java/com/mdm/mock/AccountEndpoint.java
+++ src/main/java/com/mdm/mock/AccountEndpoint.java
@@ -1,17 +1,31 @@
 package com.mdm.mock;
 
+import org.jdom2.Element;
+import org.jdom2.Namespace;
+import org.jdom2.filter.Filters;
+import org.jdom2.xpath.XPathExpression;
+import org.jdom2.xpath.XPathFactory;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
-import org.w3c.dom.Element;
 
 @Endpoint
 public class AccountEndpoint {
 	private static final String NAMESPACE_URI = "http://siebel.com/CustomUI";
-	
+	private static final String NAMESPACE_URI_ACC = "http://www.siebel.com/xml/SwiOrganizationB2BIOV5";
+    private XPathExpression<org.jdom2.Element> accountNameExpression;
+    
+    public AccountEndpoint() {
+    	Namespace namespace = Namespace.getNamespace("swi", NAMESPACE_URI_ACC);
+        XPathFactory xPathFactory = XPathFactory.instance();
+        accountNameExpression = xPathFactory.compile("//swi:ACCORB2BLegalName", Filters.element(), null, namespace);
+    }
+
 	@PayloadRoot(namespace=NAMESPACE_URI,localPart = "createOrganizationB2BV5_Input")
-	public void handleAccountRequest(@RequestPayload Element accountREquest){
-		System.out.print("ok");
+	public void handleAccountRequest(@RequestPayload Element accountRequest){
+		String name = accountNameExpression.evaluateFirst(accountRequest).getText() ;
+		
+		System.out.print("ok : "+name);
 			
 	}
 }
