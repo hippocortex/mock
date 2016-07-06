12bc016b9b4d4f76cfce359b8c79748b3690734e
hippocortex <stephane.barberye@gmail.com>
2016-07-06 16:05:46 +0200

    no message

--- src/main/java/com/accor/asa/interfaces/domain/account/siebel/xml/swiorganizationb2bio/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.siebel.com/xml/SwiOrganizationB2BIOV5", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.accor.asa.interfaces.domain.account.siebel.xml.swiorganizationb2bio;
