c58ef4b6cedf74a184f64e7fd7746c9c4b0d959a
hippocortex <stephane.barberye@gmail.com>
2016-07-06 16:05:02 +0200

    no message

--- src/main/java/com/accor/asa/interfaces/domain/account/siebel/CustomUI/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://siebel.com/CustomUI", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.accor.asa.interfaces.domain.account.siebel.CustomUI;
