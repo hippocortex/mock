29489eebe33c5ebfe5592f12ea594b06ab380066
hippocortex <stephane.barberye@gmail.com>
2016-07-08 14:20:30 +0200

    delete files

--- src/main/java/com/accor/interfaces/domain/account/siebel/CustomUI/package-info.java
+++
@@ -1,5 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://siebel.com/CustomUI",xmlns = { 
-	      @javax.xml.bind.annotation.XmlNs(prefix = "ns", 
-	    	         namespaceURI="http://siebel.com/CustomUI")
-	    	   }, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.accor.interfaces.domain.account.siebel.CustomUI;
