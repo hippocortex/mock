c9730776ea5796dd12724da1a92cfd9185ee21ce
hippocortex <stephane.barberye@gmail.com>
2016-07-08 10:58:24 +0200

    no message

--- src/main/java/com/accor/interfaces/domain/account/siebel/CustomUI/package-info.java
+++ src/main/java/com/accor/interfaces/domain/account/siebel/CustomUI/package-info.java
@@ -1,4 +1,5 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://siebel.com/CustomUI", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://siebel.com/CustomUI",xmlns = { 
+	      @javax.xml.bind.annotation.XmlNs(prefix = "ns", 
+	    	         namespaceURI="http://siebel.com/CustomUI")
+	    	   }, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package com.accor.interfaces.domain.account.siebel.CustomUI;
-
-import javax.xml.bind.annotation.XmlNs;
